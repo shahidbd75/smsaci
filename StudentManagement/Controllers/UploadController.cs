802b8f0c6e3d17479139a7ea8e5f6a3bfea7fca3
Shahidbd75 <shahidab@live.com>
2017-08-30 00:14:06 +0600

    file upload controller add

---
+++ StudentManagement/Controllers/UploadController.cs
@@ -0,0 +1,54 @@
+﻿using StudentManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace StudentManagement.Controllers
+{
+    [RoutePrefix("api/uploader")]
+    public class UploadController : ApiController
+    {
+        
+        StudentDbContext db = new StudentDbContext();
+        [Route("mediaupload")]
+        public async Task<HttpResponseMessage> PostFormData()
+        {
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            string root = HttpContext.Current.Server.MapPath("~/App_Data");
+            var provider = new MultipartFormDataStreamProvider(root);
+
+            try
+            {
+                await Request.Content.ReadAsMultipartAsync(provider);
+
+                // This illustrates how to get the file names.
+                foreach (MultipartFileData file in provider.FileData)
+                {
+                    //Trace.WriteLine(file.Headers.ContentDisposition.FileName);
+                    //Trace.WriteLine("Server file path: " + file.LocalFileName);
+                    Tutorial tutorial = new Tutorial()
+                    {
+                        TutorialName= file.LocalFileName,
+                        FileName = file.Headers.ContentDisposition.FileName                        
+                    };
+                    db.Tutorials.Add(tutorial);
+                    db.SaveChanges();
+                }
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (System.Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+    }
+}
