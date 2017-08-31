c0548c2e68af893308063e895a07161e62c7de9a
Abdul Baset Shahid <abdul.baset@atomapgroup.com>
2017-08-31 12:23:55 +0600

    Increase response limit for audio file upload

--- StudentManagement/Controllers/DocumentUploadController.cs
+++ StudentManagement/Controllers/DocumentUploadController.cs
@@ -20,72 +20,81 @@ namespace StudentManagement.Controllers
         public async Task<HttpResponseMessage> MediaUpload()
         {
             // Check if the request contains multipart/form-data.  
-            if (!Request.Content.IsMimeMultipartContent())
+            try
             {
-                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
-            }
-
-            var provider = await Request.Content.ReadAsMultipartAsync<InMemoryMultipartFormDataStreamProvider>(new InMemoryMultipartFormDataStreamProvider());
-
-            NameValueCollection formData = provider.FormData;
-
-            IList<HttpContent> files = provider.Files;
-
-            HttpContent file1 = files[0];
-            var thisFileName = file1.Headers.ContentDisposition.FileName.Trim('\"');
-
-            string filename = String.Empty;
-            Stream input = await file1.ReadAsStreamAsync();
-            string directoryName = String.Empty;
-            string URL = String.Empty;
-            string tempDocUrl = WebConfigurationManager.AppSettings["DocsUrl"];
-
-            if (formData["TutorialName"] != string.Empty && formData["StudentId"] !="")
-            {
-                var path = HttpRuntime.AppDomainAppPath;
-                directoryName = System.IO.Path.Combine(path, "ClientDocument");
-                filename = System.IO.Path.Combine(directoryName, thisFileName);
-
-                //Deletion exists file  
-                if (File.Exists(filename))
+                if (!Request.Content.IsMimeMultipartContent())
                 {
-                    File.Delete(filename);
+                    throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
                 }
 
-                string DocsPath = tempDocUrl + "/" + "ClientDocument" + "/";
-                URL = DocsPath + thisFileName;
+                var provider = await Request.Content.ReadAsMultipartAsync<InMemoryMultipartFormDataStreamProvider>(new InMemoryMultipartFormDataStreamProvider());
 
-                Tutorial tutorial = new Tutorial() {
-                    FileLocation = URL,
-                    StudentId = Convert.ToInt32(formData["StudentId"]),
-                    TutorialName = formData["TutorialName"],
-                    FileName = thisFileName
-                };
-                using(StudentDbContext db=new StudentDbContext())
+                NameValueCollection formData = provider.FormData;
+
+                IList<HttpContent> files = provider.Files;
+
+                HttpContent file1 = files[0];
+                var thisFileName = file1.Headers.ContentDisposition.FileName.Trim('\"');
+
+                string filename = String.Empty;
+                Stream input = await file1.ReadAsStreamAsync();
+                string directoryName = String.Empty;
+                string URL = String.Empty;
+                string tempDocUrl = WebConfigurationManager.AppSettings["DocsUrl"];
+
+                if (formData["TutorialName"] != string.Empty && formData["StudentId"] != "")
                 {
-                    db.Tutorials.Add(tutorial);
-                    db.SaveChanges();
+                    var path = HttpRuntime.AppDomainAppPath;
+                    directoryName = System.IO.Path.Combine(path, "ClientDocument");
+                    filename = System.IO.Path.Combine(directoryName, thisFileName);
+
+                    //Deletion exists file  
+                    if (File.Exists(filename))
+                    {
+                        File.Delete(filename);
+                    }
+
+                    string DocsPath = tempDocUrl + "/" + "ClientDocument" + "/";
+                    URL = DocsPath + thisFileName;
+
+                    Tutorial tutorial = new Tutorial()
+                    {
+                        FileLocation = URL,
+                        StudentId = Convert.ToInt32(formData["StudentId"]),
+                        TutorialName = formData["TutorialName"],
+                        FileName = thisFileName
+                    };
+                    using (StudentDbContext db = new StudentDbContext())
+                    {
+                        db.Tutorials.Add(tutorial);
+                        db.SaveChanges();
+                    }
                 }
+                else
+                {
+                    var res = Request.CreateResponse(HttpStatusCode.NoContent, "Please fill form with correct data");
+                    res.Headers.Add("DocsUrl", URL);
+                    return res;
+                }
+
+
+                //Directory.CreateDirectory(@directoryName);  
+                using (Stream file = File.OpenWrite(filename))
+                {
+                    input.CopyTo(file);
+                    //close file  
+                    file.Close();
+                }
+
+                var response = Request.CreateResponse(HttpStatusCode.OK, URL);
+                response.Headers.Add("DocsUrl", URL);
+                return response;
             }
-            else
+            catch (Exception ex)
             {
-                var res = Request.CreateResponse(HttpStatusCode.NoContent, "Please fill form with correct data");
-                res.Headers.Add("DocsUrl", URL);
-                return res;
+                var response = Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+                return response;
             }
-
-
-            //Directory.CreateDirectory(@directoryName);  
-            using (Stream file = File.OpenWrite(filename))
-            {
-                input.CopyTo(file);
-                //close file  
-                file.Close();
-            }
-
-            var response = Request.CreateResponse(HttpStatusCode.OK,"Saved Successfully");
-            response.Headers.Add("DocsUrl", URL);
-            return response;
         }
 
     }
